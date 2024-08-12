f6b4d5c8436a29ea984cb012d3d2b13472235238
gauthmz <gauthambabu12@gmail.com>
2024-08-12 22:15:57 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import AddUser from './components/AddUser';
 import HomePage from './components/HomePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddUser/>
-      <HomePage/>
-    </div>
+    <BrowserRouter>
+
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/add' element={<AddUser />} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,34 +1,29 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 const HomePage = () => {
     return (
+
         <div>
 
+            <NavBar />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-
                         <div className="row g-3">
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-
                                 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
                                     <div class="carousel-inner">
                                         <div class="carousel-item active">
-                                            <img src="https://media.istockphoto.com/id/1420039900/photo/cyber-security-ransomware-email-phishing-encrypted-technology-digital-information-protected.jpg?s=612x612&w=0&k=20&c=8wFwFVMOpW9gF2GTOx0vagIKDaw3YNFnBVbYCmoTUSY=" class="d-block w-100" alt="...">
-                                        </div>
-                                        <div class="carousel-item">
-                                            <img src="https://www.theforage.com/blog/wp-content/uploads/2022/12/what-is-cybersecurity.jpg" class="d-block w-100" alt="...">
+                                            <img src="https://images.unsplash.com/photo-1568027762272-e4da8b386fe9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="..." />
                                         </div>
                                     </div>
                                 </div>
-
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
